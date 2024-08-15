bc580155b2b90ff262214656213bac8d92ec06e9
Daehico <empire_95@mail.ru>
2024-08-16 00:19:40 +0300

    Add event service

---
+++ Assets/Scripts/EventService/Data/AnalyticEventData.cs
@@ -0,0 +1,14 @@
+﻿namespace EventService.Data
+{
+    public class AnalyticEventData
+    {
+        public string Type;
+        public string Data;
+
+        public AnalyticEventData(string type, string data)
+        {
+            Type = type;
+            Data = data;
+        }
+    }
+}
---
+++ Assets/Scripts/EventService/EventService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using EventService.Data;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace EventService
+{
+    public class EventService : MonoBehaviour
+    {
+        private float _cooldownBeforeSend = 3f;
+        private string _serverUrl = "";
+        private bool _isCooldownStart = false;
+        private List<AnalyticEventData> _analyticEventDataQueue = new List<AnalyticEventData>();
+
+        private void Start()
+        {
+            if (_analyticEventDataQueue.Count != 0)
+                SendEvent();
+        }
+
+        public void TrackEvent(string type, string data)
+        {
+            AnalyticEventData analyticEventData = new AnalyticEventData(type, data);
+            _analyticEventDataQueue.Add(analyticEventData);
+
+            if (!_isCooldownStart)
+            {
+                  SendEvent();
+                  _isCooldownStart = true;
+                  StartCoroutine(StartTimer());
+            }
+        }
+
+        private IEnumerator StartTimer()
+        {
+            yield return new WaitForSeconds(_cooldownBeforeSend);
+            _isCooldownStart = false;
+            
+            if(_analyticEventDataQueue.Count != 0)
+                SendEvent();
+        }
+
+        private void SendEvent()
+        {
+            foreach (AnalyticEventData analyticEventData in _analyticEventDataQueue)
+            {
+                StartCoroutine(SendEvent(analyticEventData));
+            }
+        }
+
+        private IEnumerator SendEvent(AnalyticEventData analyticEventData)
+        {
+            string data = JsonUtility.ToJson(analyticEventData);
+            
+            using (UnityWebRequest request = UnityWebRequest.Post(_serverUrl, data))
+            {
+                yield return request.SendWebRequest();
+
+                if (request.isHttpError || request.isNetworkError)
+                {
+                    Debug.LogError(request.error);
+                }
+                else
+                {
+                    if (request.responseCode == 200)
+                    {
+                        _analyticEventDataQueue.Remove(analyticEventData);
+                    }
+                }
+            }
+        }
+    }
+}
